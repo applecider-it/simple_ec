bdf44a75be248006d0b624d2c63970ddff6b056b
owner <owner@localhost>
2026-03-15 09:53:12 +0900

    お知らせデータ

--- src/app/services/info_services/info_service.rb
+++ src/app/services/info_services/info_service.rb
@@ -4,10 +4,20 @@ class InfoServices::InfoService
   def get_infos
     [
       {
-        title: "お知らせテスト",
-        description: "お知らせ内容 お知らせ内容 お知らせ内容 お知らせ内容 ",
-        date: "2026/3/13",
-      }
+        title: "お知らせテスト3",
+        description: "お知らせ内容 お知らせ内容 お知らせ内容 お知らせ内容 3",
+        date: "2026/6/13",
+      },
+      {
+        title: "お知らせテスト2",
+        description: "お知らせ内容 お知らせ内容 お知らせ内容 お知らせ内容 2",
+        date: "2026/5/13",
+      },
+      {
+        title: "お知らせテスト1",
+        description: "お知らせ内容 お知らせ内容 お知らせ内容 お知らせ内容 1",
+        date: "2026/4/13",
+      },
     ]
   end
 end
